fc905bc2a2b4d18d1e3fb5b9e311476a66df693a
Julien Nyambal <Julien.nyambal@gmail.com>
2025-03-20 22:20:49 +0200

    added calculator file

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class Calc:
+    def __init__(self):
+        pass
+
+    def add(self, x: int, y: int) -> int:
+        """addition of two numbers
+        
+        Args:
+            x (int): first number
+            y (int): second number
+
+        Returns:
+            int: result of addition
+        """
+        return x + y
